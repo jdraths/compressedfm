fd845e79b0b887ba1cb3c9189a04ba6305b3c345
Roane <5256269+jdraths@users.noreply.github.com>
2024-04-16 15:15:03 -0500

    tweak sample ad

--- nextjs/src/pages/sampleAd/index.js
+++ nextjs/src/pages/sampleAd/index.js
@@ -13,15 +13,9 @@ export default function Home() {
         skipTo={0}
       />
       <br />
-      <WaveformPlayer
-        artwork="https://cdn.sanity.io/images/8e5auuyh/production/7440e7d44ee70dc821863b5e87d16f35c7a63a87-500x500.png"
-        episodeTitle="Sample Ad - Zeus"
-        audioPath="/files/Zeus.mp3"
-        episodeNumber="Zeus"
-        skipTo={0}
-      />
 
       <VerticalDivider />
+      <br />
 
       <Podcatchers className="podcatchers" />
     </div>
